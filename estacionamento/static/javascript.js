8026af13fd582a44a580be38d037e81be229ebba
rafaelrvs <rafaelvieira1333@gmail.com>
2023-10-13 23:54:24 -0300

    correção da tela de pesquisa adicionando função de remover

--- estacionamento/static/javascript.js
+++ estacionamento/static/javascript.js
@@ -1,62 +1,116 @@
 document.addEventListener('DOMContentLoaded', function() {
     const pesquisar = document.querySelector('#Pesquisar');
     const cadastro = document.querySelector('#cadastro');
-    const messageDiv = document.querySelector('.message')
-    const voltar = document.querySelector('.voltar')
-    const form = document.querySelector('.form')
-
+    const messageDiv = document.querySelector('.message');
+    const voltar = document.querySelector('.voltar');
+    const form = document.querySelector('.form');
 
     cadastro.addEventListener('click', (event) => {
         /* 
-            mostra a mensagem de cadastro e atualiza a pagina automaticamente 
-            apos 2.5 segundos
+            mostra a mensagem de cadastro e atualiza a página automaticamente 
+            após 2.5 segundos
         */
         event.preventDefault();
-        const innerHtml = '<p>Veiculo cadastrado com sucesso</p>'
-        messageDiv.innerHTML = innerHtml
-        messageDiv.classList.add('message-success')
+        const innerHtml = '<p>Veículo cadastrado com sucesso</p>';
+        messageDiv.innerHTML = innerHtml;
+        messageDiv.classList.add('message-success');
         setTimeout(() => {
-            messageDiv.innerHTML=''
-            messageDiv.classList.remove('message-success')
-            form.submit()
-        }, 2500); 
+            messageDiv.innerHTML = '';
+            messageDiv.classList.remove('message-success');
+            form.submit();
+        }, 2500);
     });
 
     pesquisar.addEventListener('click', function(event) {
         event.preventDefault();
-        const div = document.querySelector('.content') // div onde mostra todos carros
-        div.style.visibility = 'visible'  
-        voltar.style.visibility = 'visible' //botão de voltar
-
+        const div = document.querySelector('.content'); // div onde mostra todos os carros
+        div.style.visibility = 'visible';
+        voltar.style.visibility = 'visible'; // botão de voltar
 
         fetch(`${window.location.href}/carros`).then(response => {
-            console.log(response)
-            //  esse primeiro then vai retorna o objeto completo da solicitação http
-            return response.json()
+            return response.json();
         }).then(data => {
-            // já esse segundo then vai ter os dados ja processados, para um json
             data.forEach(element => {
                 const novaLinha = document.createElement('tr');
                 novaLinha.innerHTML =
-                ` 
-                <td>${element.carro}</td>
-                <td>${element.placa}</td>
-                <td>${element.data_entrada}</td>
+                    `<td id="carro">${element.carro}</td>
+                    <td id="placa" >${element.placa}</td>
+                    <td id="dataDeEntrada" >${element.data_entrada}</td>`;
 
-                `
-                console.log(element.data_entrada)
                 div.appendChild(novaLinha);
+                if (!novaLinha.onclick === true) {
+                    selecionaLinha(novaLinha, data);
+                }
             });
         })
-        .catch(error =>{
-            // essa parte vc ignora, dev bom n deixa erros acontecer hehe
+        .catch(error => {
             console.error('Erro ao buscar os carros:', error);
-        })
-
+        });
     });
-    voltar.addEventListener('click', () =>{
-        // redireciona para a pagina do formulário quando vc clica no botão voltar
-        window.location.href = window.location.href
-    })
 
+    voltar.addEventListener('click', () => {
+        // redireciona para a página do formulário quando você clica no botão voltar
+        window.location.href = window.location.href;
+    });
 });
+
+function selecionaLinha(novaLinha, data) {
+    novaLinha.addEventListener('click', (event) => {
+        const clickForaDaPlaca = event.target.id;
+        if (clickForaDaPlaca === "placa") {
+            novaLinha.style.background = "red";
+            const placa = event.target.innerHTML;
+            mostrarData(data, placa);
+            novaLinha.remove(   )
+        } else {
+            alert('Clique na placa para remover o veículo e cobrar o valor');
+        }
+    });
+}
+
+function mostrarData(data, placa) {
+    const dataDeEntrada = encontrarDataEntrada(placa, data);
+    if (dataDeEntrada) {
+        const valorACobrar = calcularValorACobrar(dataDeEntrada);
+        alert(`Data de entrada para a placa ${placa}: ${dataDeEntrada} - Valor a cobrar: R$ ${valorACobrar.toFixed(2)}`);
+        
+    } else {
+        alert(`Data de entrada não encontrada para a placa ${placa}`);
+    }
+}
+
+function encontrarDataEntrada(placa, data) {
+    for (const element of data) {
+        if (element.placa === placa) {
+            return element.data_entrada;
+        }
+    }
+    return null;
+}
+
+function calcularValorACobrar(dataDeEntrada) {
+    const dataEntrada = new Date(parseDataHora(dataDeEntrada));
+    const dataAtual = new Date();
+    
+    const diferencaEmMilissegundos = dataAtual - dataEntrada;
+    const diferencaEmHoras = diferencaEmMilissegundos / (1000 * 60 * 60);
+    
+    const valorHora = 4.80;
+    const valorACobrar = valorHora * diferencaEmHoras;
+    
+    return valorACobrar;
+}
+
+function parseDataHora(dataHoraString) {
+    const partes = dataHoraString.split(" ");
+    const dataPartes = partes[0].split("/");
+    const horaPartes = partes[1].split(":");
+    
+    const dia = parseInt(dataPartes[0]);
+    const mes = parseInt(dataPartes[1]) - 1;
+    const ano = parseInt(dataPartes[2]);
+    const hora = parseInt(horaPartes[0]);
+    const minutos = parseInt(horaPartes[1]);
+    
+    return new Date(ano, mes, dia, hora, minutos);
+}
